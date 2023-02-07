09601294be35667d8f69810dcf93b0f8f163088d
Michael La Dow <michael.ladow@kolmeo.com>
2023-02-07 12:04:34 +1100

    initcommit

---
+++ MLDev.LOTOW/Models/Stat.cs
@@ -0,0 +1,10 @@
+﻿namespace MLDev.LOTOW.Models
+{
+    public class Stat
+    {
+        public int StatId { get; set; }
+        public string Name { get; set; }
+        public int Value { get; set; }
+        public StatModifier[] Modifiers { get; set; }
+    }
+}
---
+++ MLDev.LOTOW/Models/StatModifier.cs
@@ -0,0 +1,9 @@
+﻿namespace MLDev.LOTOW.Models
+{
+    public class StatModifier
+    {
+        public int StatModifierId { get; set; }
+        public int Value { get; set; }
+        public string SourceDescription { get; set; }
+    }
+}
